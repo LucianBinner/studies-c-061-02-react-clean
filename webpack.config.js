a0472a9111b10ef11f35d9e0f1f07a59316f2f2c
Binner <lucian.binner@hotmail.com>
2021-08-11 09:55:37 -0300

    feat: add login view

--- webpack.config.js
+++ webpack.config.js
@@ -7,41 +7,45 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'public/js'),
     publicPath: '/public/js',
-    fileName: 'bundle.js'
+    filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', 'scss'],
+    extensions: ['.ts', '.tsx', '.js', '.scss'],
     alias: {
       '@': path.join(__dirname, 'src')
     }
   },
   module: {
-    rules: [{
-      test: /\.ts(x?)$/,
-      loader: 'ts-loader',
-      exclude: /node_modules/
-    }, {
-      test: /\.scss$/,
-      use: [{
-        loader: 'style-loader'
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
       }, {
-        loader: 'css-loader',
-        options: {
-          module: true
-        }
-      }, {
-        loader: 'sass-loader'
-      }]
-    }]
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          }, {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          }, {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
   },
   devServer: {
-    contentBase: './public',
+    contentBase: path.join(__dirname, 'public'),
     writeToDisk: true,
     historyApiFallback: true
   },
   externals: {
     react: 'React',
-    'react-dom': 'ReactDom'
+    'react-dom': 'ReactDOM'
   },
   plugins: [
     new CleanWebpackPlugin()
