fadb7e05981ae4e501faef1628fd7c5f72c650ae
Binner <lucian.binner@hotmail.com>
2021-08-13 08:29:03 -0300

    refactor: move ValidationSpy to its own file

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -2,20 +2,7 @@ import React from 'react'
 import { render, RenderResult, fireEvent, cleanup } from '@testing-library/react'
 import Login from './login'
 import { Validation } from '@/presentation/protocols/validation'
-
-const mockValidation = (): Validation => {
-  class ValidationSpy implements Validation {
-    errorMessage: string
-    fieldName: string
-    fieldValue: string
-    validate(fieldName: string, fieldValue: string): string {
-      this.fieldName = fieldName
-      this.fieldValue = fieldValue
-      return this.errorMessage
-    }
-  }
-  return new ValidationSpy()
-}
+import { mockValidation } from '@/presentation/test'
 
 type SutTypes = {
   sut: RenderResult
