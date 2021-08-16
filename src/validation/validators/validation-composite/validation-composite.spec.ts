41b6506ffc82dd23a9da763a370ea93848b6cb05
Binner <lucian.binner@hotmail.com>
2021-08-16 17:34:48 -0300

    test: ensure ValidationComposite returns the first error found

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -4,10 +4,11 @@ import { ValidationComposite } from './validation-composite'
 describe('ValidationComposite', () => {
   test('Should return error if any validation fails', () => {
     const fieldValidationSpy = mockFieldValidation('any_field')
+    fieldValidationSpy.error = new Error('first_error_message')
     const fieldValidationSpyTwo = mockFieldValidation('any_field')
-    fieldValidationSpyTwo.error = new Error('any_error_message')
+    fieldValidationSpyTwo.error = new Error('second_error_message')
     const sut = new ValidationComposite([fieldValidationSpy, fieldValidationSpyTwo])
     const error = sut.validate('any_field', 'any_value')
-    expect(error).toBe('any_error_message')
+    expect(error).toBe('first_error_message')
   })
 })
