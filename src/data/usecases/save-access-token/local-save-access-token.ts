4801a71589da28bd37b0c2e7ff3186ea6511183f
Binner <lucian.binner@hotmail.com>
2021-08-17 13:29:03 -0300

    test: ensure LocalSaveAccessToken calls SetStorage with correct value

--- src/data/usecases/save-access-token/local-save-access-token.ts
+++ src/data/usecases/save-access-token/local-save-access-token.ts
@@ -1,10 +1,9 @@
 import { SetStorage } from '@/data/protocols/cache/set-storage'
+import { SaveAccessToken } from '@/domain/usecases/save-access-token'
 
-export class SetStorageSpy implements SetStorage {
-  key: string
-  value: any
-  async set(key: string, value: any): Promise<void> {
-    this.key = key
-    this.value = value
+export class LocalSaveAccessToken implements SaveAccessToken {
+  constructor(private readonly setStorage: SetStorage) { }
+  async save(accessToken: string): Promise<void> {
+    await this.setStorage.set('accessToken', accessToken)
   }
 }
