36abeacc4c28d7aed066dee7f22149bcf157663b
Binner <lucian.binner@hotmail.com>
2021-08-24 09:39:03 -0300

    refactor: move Router to Main Layer

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,6 @@ module.exports = {
   collectCoverageFrom: [
     '<rootDir>/src/**/*.{ts,tsx}',
     '!<rootDir>/src/main/**/*',
-    '!<rootDir>/src/presentation/components/router/**/*',
     '!<rootDir>/src/presentation/pages/index.ts',
     '!<rootDir>/src/domain/**/index.ts',
     '!<rootDir>/src/presentation/components/index.ts',
--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,14 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router } from '@/presentation/components'
+import Router from './routes/routes'
 import '@/presentation/styles/global.scss'
-import { makeLogin } from '@/main/factories/pages/login/login-factory'
-import { makeSignUp } from './factories/pages/signup/signup-factory'
 
 ReactDOM.render(
-  <Router
-    makeLogin={makeLogin}
-    makeSignup={makeSignUp}
-  />,
+  <Router/>,
   document.getElementById('main')
 )
