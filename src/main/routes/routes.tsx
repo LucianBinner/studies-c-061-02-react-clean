ec907078e6312490ffa49e603a1becf6859dd0b7
Binner <lucian.binner@hotmail.com>
2021-08-25 08:57:48 -0300

    feat: add getCurrentAccount to ApiContext

--- src/main/routes/routes.tsx
+++ src/main/routes/routes.tsx
@@ -1,10 +1,10 @@
+import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { setCurrentAccountAdapter } from '@/main/adapters/current-account-adapter'
 import { makeLogin } from '@/main/factories/pages/login/login-factory'
 import { makeSignUp } from '@/main/factories/pages/signup/signup-factory'
 import { ApiContext } from '@/presentation/contexts'
 import { SurveyList } from '@/presentation/pages'
-import React from 'react'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import { setCurrentAccountAdapter } from '../adapters/current-account-adapter'
 
 const Router: React.FC = () => {
   return (
