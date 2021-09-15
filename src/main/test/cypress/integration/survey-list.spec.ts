47ec351f6a16bde037c71cc374db05609449f437
Binner <lucian.binner@hotmail.com>
2021-09-15 12:28:50 -0300

    test: ensure SurveyList logout on logout link click

--- src/main/test/cypress/integration/survey-list.spec.ts
+++ src/main/test/cypress/integration/survey-list.spec.ts
@@ -18,4 +18,11 @@ describe('SurveyList', () => {
     cy.visit('')
     Helper.testUrl('/login')
   })
+
+  it('Should logout on logout link click', () => {
+    Http.mockUnexpectedError()
+    cy.visit('')
+    cy.getByTestId('logout').click()
+    Helper.testUrl('/login')
+  })
 })
