cf7aec30c9c5826a070513bbb8fbf30e495b50f2
Binner <lucian.binner@hotmail.com>
2021-09-15 12:21:12 -0300

    test: ensure SurveyList logout on AccessDeniedError

--- src/main/test/cypress/support/survey-list-mocks.ts
+++ src/main/test/cypress/support/survey-list-mocks.ts
@@ -2,3 +2,6 @@ import * as Http from './http-mocks'
 
 export const mockUnexpectedError = (): void =>
   Http.mockServerError(/surveys/, 'GET')
+
+export const mocAccessDeniedError = (): void =>
+  Http.mockForbiddenError(/surveys/, 'GET')
